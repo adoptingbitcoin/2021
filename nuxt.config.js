1511631d02a80c7d41dc8058f1b4e72901a87317
Kai Reichart <kai@reichart.dev>
2021-08-27 16:31:20 +0200

    Page Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'adopting-bitcoin',
+    title: 'Adopting Bitcoin',
     htmlAttrs: {
       lang: 'en'
     },
