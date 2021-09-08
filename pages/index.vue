8e0279cf73440bec6a247a5b9f361edd664f74dd
Kai Reichart <kai@reichart.dev>
2021-09-08 17:40:19 +0300

    added url
    Signed-off-by: Kai Reichart <kai@reichart.dev>

--- pages/index.vue
+++ pages/index.vue
@@ -30,7 +30,7 @@ export default {
   components: { Sponsors, Tickets, OrganizerList, ContactUs, Subscribe, Locations, Schedule, ComeJoinUs, SpeakerList, JoinTheNetwork, Header },
   async asyncData ({ $content, params, i18n }) {
     const speakers = await $content('speakers', params.slug)
-      .only(['name', 'function', 'img', 'slug', 'showPage', 'twitter'])
+      .only(['name', 'function', 'img', 'slug', 'showPage', 'twitter', 'url'])
       .sortBy('prio', 'asc')
       .fetch()
 
